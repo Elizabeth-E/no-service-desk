3e598d65ed18362cdfc15bdeb4c185795a82f945
Elizabeth <elizabeth.erickson21@gmail.com>
2025-11-02 14:40:09 +0100

    added views for dashboard (service desk employee) and login screen

--- src/main/java/nl/inholland/student/noservicedesk/NoServiceDeskApplication.java
+++ src/main/java/nl/inholland/student/noservicedesk/NoServiceDeskApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class NoServiceDeskApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(NoServiceDeskApplication.class.getResource("NoServiceDeskLogin-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
