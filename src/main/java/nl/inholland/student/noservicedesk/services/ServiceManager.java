519177d1e7f6de97d700201e04f2772e61c717b4
Elizabeth <elizabeth.erickson21@gmail.com>
2025-11-14 12:47:29 +0100

    Revert "fixed error with the controller in the login view"
    This reverts commit 98c8d0a6a978ee60b32a2bf046ad1458377684d4.

--- src/main/java/nl/inholland/student/noservicedesk/services/ServiceManager.java
+++ src/main/java/nl/inholland/student/noservicedesk/services/ServiceManager.java
@@ -30,8 +30,4 @@ public class ServiceManager {
     public HandledTicketsService getHandledTicketsService() {
         return handledTicketsService;
     }
-
-    public void conndb() {
-        db.connectDB();
-    }
 }
