c9d8a326b44fc4d0cfdc5c26882112d49d63455d
Elizabeth <elizabeth.erickson21@gmail.com>
2026-01-05 23:24:16 +0100

    changed navigation, added login controller, createnewticket controller and cleaned up mainviewcontroller to only handle the view. no more crazy stage passing

---
+++ src/main/java/nl/inholland/student/noservicedesk/Controllers/MainViewController.java
@@ -0,0 +1,75 @@
+package nl.inholland.student.noservicedesk.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import nl.inholland.student.noservicedesk.NoServiceDeskApplication;
+import nl.inholland.student.noservicedesk.services.ServiceManager;
+
+public class MainViewController {
+
+    @FXML private BorderPane root; // must exist in Main-view.fxml as fx:id="viewLayout"
+
+    private Stage stage;
+    private ServiceManager serviceManager;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void setServiceManager(ServiceManager serviceManager) {
+        this.serviceManager = serviceManager;
+    }
+
+    @FXML
+    public void showDashboard() {
+        setCenter("Dashboard-view.fxml", controller -> {
+            if (controller instanceof DashboardController dc) {
+                dc.setServiceManager(serviceManager);
+                dc.setMainViewController(this);
+                dc.buildDashboard();
+            }
+        });
+        if (stage != null) stage.setTitle("Dashboard");
+    }
+
+    @FXML
+    public void showTickets() {
+        setCenter("TicketsOverview-view.fxml", controller -> {
+            if (controller instanceof TicketsOverviewController tc) {
+                tc.setServiceManager(serviceManager);
+                tc.setMainViewController(this);
+                tc.fillTicketsTable();
+            }
+        });
+        if (stage != null) stage.setTitle("Tickets Overview");
+    }
+
+    @FXML
+    public void showCreateIncident() {
+        setCenter("CreateNewTicket-view.fxml", controller -> {
+            if (controller instanceof CreateNewTicketController cc) {
+                cc.setServiceManager(serviceManager);
+                cc.setMainViewController(this);
+                cc.buildCreateNewTicketForm();
+            }
+        });
+        if (stage != null) stage.setTitle("Create Ticket");
+    }
+
+    private void setCenter(String fxml, java.util.function.Consumer<Object> afterLoad) {
+        try {
+            FXMLLoader loader = new FXMLLoader(NoServiceDeskApplication.class.getResource(fxml));
+            Parent view = loader.load();
+            Object controller = loader.getController();
+
+            afterLoad.accept(controller);
+            root.setCenter(view);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
